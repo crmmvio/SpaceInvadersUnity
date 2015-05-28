4f1671f2c95c3de397826171d877b528d02e792c
Abraão Zaidan <abraao@cakeweb.com.br>
2015-05-28 20:46:01 -0300

    Nave inimiga explode antes de sumir

--- SpaceInvaders/Assets/Scripts/NaveInimiga.cs
+++ SpaceInvaders/Assets/Scripts/NaveInimiga.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class NaveInimiga : MonoBehaviour
 {
+	public GameObject explosao;
+	public float delayExplosao = 2.5f;
+
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.gameObject.tag == "Bullet")
 		{
+			Instantiate(explosao, transform.position, Quaternion.identity);
 			Destroy(col.gameObject);
-			Destroy(gameObject);
+			Destroy(gameObject, delayExplosao);
 		}
 	}
 }
